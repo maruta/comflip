c37675cea497069e98858730d44133004b75bc6b
Ichiro Maruta <ichiro.maruta@gmail.com>
2021-05-14 11:17:57 +0900

    update home

--- public/home.js
+++ public/home.js
@@ -3,16 +3,22 @@ document.addEventListener('DOMContentLoaded', function (event) {
     const emitterLink = document.getElementById("emitter-link");
     const rendererLink = document.getElementById("renderer-link");
     const qrcodeElm = document.getElementById("qrcode");
-
+    const emitterIframe = document.getElementById("emitter-iframe");
+    const rendererIframe = document.getElementById("renderer-iframe");
+    const hostElm = document.getElementById("host");
+    hostElm.innerText = location.host;
+    
     const updateLinks = () => {
         const roomName = roomNameInput.value;
         const emitterURL = new URL("/e#"+roomName,document.baseURI);
         emitterLink.setAttribute('href',emitterURL.href);
         emitterLink.innerText = emitterURL.href;
+        emitterIframe.src = emitterURL.href;
         QRCode.toCanvas(qrcodeElm, emitterURL.href);
         const rendererURL = new URL("/renderer#"+roomName,document.baseURI);
         rendererLink.setAttribute('href',rendererURL.href);
         rendererLink.innerText = rendererURL.href;
+        rendererIframe.src = rendererURL.href;        
     }
 
     updateLinks();
