cc4d9e927f6f4c1fef19cf58a9609aec2e73efbb
Ichiro Maruta <ichiro.maruta@gmail.com>
2021-05-16 23:44:46 +0900

    Create an English version

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const socketIO = require('socket.io');
+const url = require('url');
 
 const PORT = process.env.PORT || 3000;
 
+const rootUrl = (req) => {
+    let s = Object.assign(new URL("http://example.com/"), {
+        protocol: req.protocol,
+        host: req.get('host')
+    });
+    return s;
+};
+
 const server = express()
     .use(express.json())
+    .set('views', __dirname + '/views')
+    .engine('html', require('ejs').renderFile)
+    .set('view engine', 'html')
     .set('trust proxy', true)
     .post('/api/emit', (req, res) => {
         console.log({
@@ -12,7 +24,7 @@ const server = express()
             room: req.body.room,
             comment: req.body.comment
         });
-        io.to(req.body.room).emit('comment',req.body.comment);
+        io.to(req.body.room).emit('comment', req.body.comment);
         res.send("ok");
     })
     .get('/api/heartbeat', function (req, res) {
@@ -22,6 +34,18 @@ const server = express()
             room: req.query.room
         });
         res.send('ok');
+    })
+    .get("/", function (req, res) {
+        const data = {
+            rootUrl: rootUrl(req)
+        };
+        res.render("./index.en.ejs", data);
+    })
+    .get("/ja/", function (req, res) {
+        const data = {
+            rootUrl: rootUrl(req)
+        };
+        res.render("./index.ja.ejs", data);
     })
     .use(express.static('public'))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
